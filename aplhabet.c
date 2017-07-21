e4382f30acf9b316e6fb898f8328d31a8cfb78d6
Balakrishnan <30314360+Srikrishna1297@users.noreply.github.com>
2017-07-21 16:00:52 +0530

    Create aplhabet.c

---
+++ aplhabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   char a;
+   scanf("%c",&a);
+  if(isalpha(a))
+   {
+       printf("albhabet");
+   }
+   else {
+           printf("not albhapet");
+   }
+   return 0;
+}
