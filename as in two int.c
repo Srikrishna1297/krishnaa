679eebb39df1f483de7b4f0ceab40f66afa918df
Balakrishnan <30314360+Srikrishna1297@users.noreply.github.com>
2017-08-12 16:43:58 +0530

    Update as in two int.c

--- as in two int.c
+++ as in two int.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 int main()
 {
-  int n1, n2, i, temp, num, rem;
+  int n1, n2, i,t,n,r;
   printf("Enter two numbers(intervals): ");
   scanf("%d %d", &n1, &n2);
   printf("Armstrong numbers between %d an %d are: ", n1, n2);
   for(i=n1+1; i<n2; ++i)
   {
-      temp=i;
-      num=0;
-      while(temp!=0)
+      t=i;
+      n=0;
+      while(t!=0)
       {
-          rem=(temp%10);
-          num+=rem*rem*rem;
-          temp/=10;
+          r=(t%10);
+          n+=r*r*r;
+          t/=10;
       }
-      if(i==num)
+      if(i==n)
       {
           printf("%d ",i);
       }
