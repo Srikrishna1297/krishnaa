82f2cb53ddad8745d9890494ffe34224ee01937e
Balakrishnan <30314360+Srikrishna1297@users.noreply.github.com>
2017-08-12 16:20:15 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n, sum = 0, rem = 0, cube = 0, temp;
+
+    printf ("enter a number");
+    scanf("%d",&n);
+    temp = n;
+    while (n!= 0)
+    {
+        rem=n%10;
+        cube=pow(rem,3);
+        sum = sum+cube;
+        n=n/10;
+    }
+    if (sum==temp)
+        printf ("The given no is armstrong no");
+    else
+     printf ("The given no is not a armstrong no");
+     return 0;
+}
+
